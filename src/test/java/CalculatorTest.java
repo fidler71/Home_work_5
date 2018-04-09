ed56c847bcda5ffeb95a8e8dcfa07cea2d98b922
Ivan Ionov <IAIonov.SBT@sberbank.ru>
2018-04-09 15:15:14 +0300

    !!!

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by user on 09.04.2018.
+ */
+class CalculatorTest {
+	@org.junit.jupiter.api.Test
+	void main() {
+		
+
+	}
+
+}
